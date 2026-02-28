8e340b830e0af5afa56a12650c6814a177d93553
KilehKa <KilehKa+github@proton.me>
2026-02-28 12:41:18 -0500

    Initial Server Test
    Initial ruleset and format additions (working status unknown)

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -7,7 +7,79 @@ export const Rulesets: import('../sim/dex-formats').FormatDataTable = {
 
 	// Rulesets
 	///////////////////////////////////////////////////////////////////
+	otlcube: {
+		effectType: 'ValidatorRule',
+		name: 'OTL Cube',
+		desc: 'Applies the rules for the OTL Cube Draft format.',
+		ruleset: [
+			'Sleep Clause Mod', 'Species Clause', 'Nickname Clause', 'OHKO Clause', 'Evasion Items Clause', 'Evasion Moves Clause',
+			'-Released', '-Unreleased', '-Unobtainable', '-Nonexistent', '-terastallization',
+			'-toxic', '-scald', '-defog', '-rapidspin', '-stickyweb', '-stealthrock', '-spikes', '-toxicspikes',
+			'-Eviolite', '-Heavy-duty boots',
+			'+Salazzle + toxic', '+Simipour + scald', '+Vullaby + defog', '+Illumise + defog', '+Whimsicott + defog', 
+			'+Vespiquen + defog','+Claydol + rapidspin', '+Morpeko + rapidspin', '+Coalossal + rapidspin', '+Tatsugiri + rapidspin',
+			'+Cradily + stealthrock', '+Stunfisk-Galar + stealthrock', '+Mawile + stealthrock', '+Golisopod + spikes', 
+			'+Cofagrigus + toxicspikes', '+Garbodor + toxicspikes', '+Vullaby + Eviolite', '+Gligar + Eviolite',
+			'+Centiskorch + Heavy-Duty Boots',
 
+			'+Froslass', '+Archeops', '+Fezandipiti', '+Sigilyph', '+Marowak-Alola', '+Lanturn','+Decidueye',
+			'+Bewear', '+Mega Camerupt', '+Duraludon', '+Ribombee', '+Qwilfish-Hisui','+Mega Audino','+Claydol',
+			'+Morpeko','+Cradily','+Veluza','+Noivern','+Pinsir','+Lucario','+Crabominable','+Houndoom','+Zygarde-10%',
+			'+Weezing-Galar','+Avalugg-Hisui','+Golisopod','+Arboliva','+Togedemaru','+Vullaby','+Dachsbun',
+			'+Rotom-Frost','+Salazzle','+Calyrex','+Zoroark-Hisui','+Stunfisk-Galar','+Simipour','+Illumise',
+			'+Virizion','+Druddigon','+Dragalge','+Gligar','+Coalossal','+Vikavolt','+Empoleon','+Articuno','+Zangoose',
+			'+Palossand','+Slowbro-Galar','+Pangoro','+Klawf','+Sableye','+Centiskorch','+Gallade','+Tatsugiri',
+			'+Golem-Alola','+Mawile','+Tsareena','+Glaceon','+Tyrantrum','+Heliolisk','+Delphox','+Quagsire',
+			'+Whimsicott','+Magneton','+Sneasel','+Toxicroak','+Vespiquen','+Cofagrigus','+Braviary','+Garbodor',
+			'+Lokix','+Lunatone','+Poliwrath','+Entei','+Dugtrio-Alola','+Mimikyu','+Drampa','+Mega Abomasnow',
+		],
+		// Mostly hardcoded in team-validator.ts
+		onValidateTeam(team, format) {
+			let kyuremCount = 0;
+			for (const set of team) {
+				if (set.species === 'Kyurem-White' || set.species === 'Kyurem-Black') {
+					if (kyuremCount > 0) {
+						return [
+							`You cannot have more than one Kyurem-Black/Kyurem-White.`,
+							`(It's untradeable and you can only make one with the DNA Splicers.)`,
+						];
+					}
+					kyuremCount++;
+				}
+				if (set.moves.includes('defog') && !['Vullaby', 'Illumise', 'Whimsicott', 'Vespiquen'].includes(set.species)) {
+					return [`Only Vullaby, Illumise, Whimsicott, and Vespiquen can use Defog.`];
+				}
+				if (set.moves.includes('rapidspin') && !['Claydol', 'Morpeko', 'Coalossal', 'Tatsugiri'].includes(set.species)) {
+					return [`Only Claydol, Morpeko, Coalossal, and Tatsugiri can use Rapid Spin.`];
+				}
+				if (set.moves.includes('stickyweb')) {
+					return ['Sticky Web is banned.']
+				}
+				if (set.moves.includes('stealthrock') && !['Cradily', 'Stunfisk-Galar', 'Mawile'].includes(set.species)) {
+					return [`Only Cradily, Stunfisk-Galar, and Mawile can use Stealth Rock.`];
+				}
+				if (set.moves.includes('spikes') && !['Golisopod'].includes(set.species)) {
+					return [`Only Golisopod can use Spikes.`];
+				}
+				if (set.moves.includes('toxicspikes') && !['Cofagrigus', 'Garbodor'].includes(set.species)) {
+					return [`Only Cofagrigus and Garbodor can use Toxic Spikes.`];
+				}
+				if (set.moves.includes('toxic') && !['Salazzle'].includes(set.species)) {
+					return [`Only Salazzle can use Toxic.`];
+				}
+				if (set.moves.includes('scald') && !['Simipour'].includes(set.species)) {
+					return [`Only Simipour can use Scald.`];
+				}
+				if (set.item === 'Eviolite' && !['Vullaby', 'Gligar'].includes(set.species)) {
+					return [`Only Vullaby and Gligar can use Eviolite.`];
+				}
+				if (set.item === 'Heavy-Duty Boots' && !['Centiskorch'].includes(set.species)) {	
+					return [`Only Centiskorch can use Heavy-Duty Boots.`];
+				}
+			}
+			return [];
+		},
+	},
 	standardag: {
 		effectType: 'ValidatorRule',
 		name: 'Standard AG',
